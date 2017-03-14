1b7cc8e38b153808977fb417298847ee8975f35d
K. Y. Srinivasan <kys@microsoft.com>
2017-03-14 16:03:13 -0700

    hv: Initialize the distro ID for the MSFT kernel
    0x30 is chosen as the distro ID for the MSFT kernel.
    Do not upstream this patch.
    
    Signed-off-by: K. Y. Srinivasan <kys@microsoft.com>

--- arch/x86/hyperv/hv_init.c
+++ arch/x86/hyperv/hv_init.c
@@ -99,7 +99,7 @@ void hyperv_init(void)
 	 * 1. Register the guest ID
 	 * 2. Enable the hypercall and register the hypercall page
 	 */
-	guest_id = generate_guest_id(0, LINUX_VERSION_CODE, 0);
+	guest_id = generate_guest_id(0x30, LINUX_VERSION_CODE, 0);
 	wrmsrl(HV_X64_MSR_GUEST_OS_ID, guest_id);
 
 	hypercall_pg  = __vmalloc(PAGE_SIZE, GFP_KERNEL, PAGE_KERNEL_RX);
