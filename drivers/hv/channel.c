6369d8549ab52840a68323a675638ae986c24b1a
Dexuan Cui <decui@microsoft.com>
2017-03-24 20:53:18 +0800

    vmbus: dynamically enqueue/dequeue the channel on vmbus_open/close
    Signed-off-by: Dexuan Cui <decui@microsoft.com>

--- drivers/hv/channel.c
+++ drivers/hv/channel.c
@@ -180,6 +180,10 @@ int vmbus_open(struct vmbus_channel *newchannel, u32 send_ringbuffer_size,
 		      &vmbus_connection.chn_msg_list);
 	spin_unlock_irqrestore(&vmbus_connection.channelmsg_lock, flags);
 
+	hv_event_tasklet_disable(newchannel);
+	hv_percpu_channel_enq(newchannel);
+	hv_event_tasklet_enable(newchannel);
+
 	ret = vmbus_post_msg(open_msg,
 			       sizeof(struct vmbus_channel_open_channel));
 	if (ret == 0)
@@ -191,18 +195,22 @@ int vmbus_open(struct vmbus_channel *newchannel, u32 send_ringbuffer_size,
 
 	if (ret != 0) {
 		err = ret;
-		goto error_free_gpadl;
+		goto error_deq_channel;
 	}
 
 	if (open_info->response.open_result.status) {
 		err = -EAGAIN;
-		goto error_free_gpadl;
+		goto error_deq_channel;
 	}
 
 	newchannel->state = CHANNEL_OPENED_STATE;
 	kfree(open_info);
 	return 0;
 
+error_deq_channel:
+	hv_event_tasklet_disable(newchannel);
+	hv_percpu_channel_deq(newchannel);
+	hv_event_tasklet_enable(newchannel);
 error_free_gpadl:
 	vmbus_teardown_gpadl(newchannel, newchannel->ringbuffer_gpadlhandle);
 	kfree(open_info);
@@ -532,6 +540,8 @@ static int vmbus_close_internal(struct vmbus_channel *channel)
 		ret = -EINVAL;
 		goto out;
 	}
+
+	hv_percpu_channel_deq(channel);
 
 	channel->state = CHANNEL_OPEN_STATE;
 	channel->sc_creation_callback = NULL;
