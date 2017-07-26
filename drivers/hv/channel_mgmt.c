010ea9ecd386f5a7205193774b9161079a1c6de0
Dexuan Cui <decui@microsoft.com>
2017-07-26 12:32:14 -0600

    vmbus: don't acquire the mutex in vmbus_hvsock_device_unregister()
    This is a tempoary workaround to avoid deadlock:
    
    If we get the mutex in vmbus_hvsock_device_unregister() and call
    vmbus_device_unregister() -> device_unregister() -> ... -> device_release()
    -> vmbus_device_release(), we'll get a deadlock, because
    vmbus_device_release() tries to get the same lock.
    
    We need to add a proper sync mechanism for onoffer, onrescind and
    vmbus_hvsock_device_unregister().
    
    Signed-off-by: Dexuan Cui <decui@microsoft.com>
    Cc: K. Y. Srinivasan <kys@microsoft.com>
    Cc: Haiyang Zhang <haiyangz@microsoft.com>
    Cc: Stephen Hemminger <sthemmin@microsoft.com>

--- drivers/hv/channel_mgmt.c
+++ drivers/hv/channel_mgmt.c
@@ -913,14 +913,11 @@ static void vmbus_onoffer_rescind(struct vmbus_channel_message_header *hdr)
 
 void vmbus_hvsock_device_unregister(struct vmbus_channel *channel)
 {
-	mutex_lock(&vmbus_connection.channel_mutex);
-
 	BUG_ON(!is_hvsock_channel(channel));
 
+	/* XXX: need to sync with onoffer/onrescind */
 	channel->rescind = true;
 	vmbus_device_unregister(channel->device_obj);
-
-	mutex_unlock(&vmbus_connection.channel_mutex);
 }
 EXPORT_SYMBOL_GPL(vmbus_hvsock_device_unregister);
 
